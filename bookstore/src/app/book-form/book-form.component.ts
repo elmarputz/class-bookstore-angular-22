943700c30602601145a2946c5b7cee1da1f21cca
Elmar Putz <ep@studiomitte.com>
2022-04-08 12:16:37 +0200

    update book

--- bookstore/src/app/book-form/book-form.component.ts
+++ bookstore/src/app/book-form/book-form.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Book } from '../shared/book';
 import { BookFactory } from '../shared/book-factory';
 import { BookStoreService } from '../shared/book-store.service';
+import { BookFormErrorMessages } from './book-form-error-messages';
 
 @Component({
   selector: 'bs-book-form',
@@ -45,9 +47,10 @@ export class BookFormComponent implements OnInit {
 
   initBook() {
 
+    this.buildThumbnailsArray();
     this.bookForm = this.fb.group({
       id: this.book.id, 
-      title: this.book.title,
+      title: [this.book.title, Validators.required], 
       subtitle: this.book.subtitle, 
       isbn: [
         this.book.isbn, [
@@ -63,24 +66,75 @@ export class BookFormComponent implements OnInit {
           Validators.max(10)
         ]
       ],
-      image: this.images,
+      images: this.images,
       published: [this.book.published, Validators.required]
     });
 
     this.bookForm.statusChanges.subscribe(() => 
       this.updateErrorMessages()
     )
-
-  
   }
+
+
+  addThumbnailControl() {
+    this.images.push(this.fb.group({ id: 0, url: null, title: null}));
+  }
+
+  buildThumbnailsArray() {
+    if (this.book.images) {
+      this.images = this.fb.array([]);
+      for (let img of this.book.images) {
+        let fg = this.fb.group({
+          id: new FormControl(img.id),
+          url: new FormControl(img.url, [Validators.required]),
+          title: new FormControl(img.title, [Validators.required])
+        });
+        this.images.push(fg);
+      }
+    }
+  }
+
 
   updateErrorMessages() {
     console.log("Is invalid? " + this.bookForm.invalid);
     this.errors = {};
+
+    for (const message of BookFormErrorMessages) {
+      const control = this.bookForm.get(message.forControl);
+      if (
+
+        control && 
+        control.dirty && 
+        control.invalid && control.errors && 
+        control.errors[message.forValidator] && 
+        !this.errors[message.forControl]
+      ) {
+        this.errors[message.forControl] = message.text;
+      } 
+    }  
+
+  
   }
 
+
+
   submitForm() {
-    
+    this.bookForm.value.images = this.bookForm.value.images.filter(
+      (thumbnail: { url: string; }) => thumbnail.url
+    );
+
+    const book: Book = BookFactory.fromObject(this.bookForm.value);  
+
+    book.authors = this.book.authors;
+
+    if (this.isUpdatingBook) {
+      this.bs.update(book).subscribe(res => {
+        this.router.navigate(['../../books', book.isbn], {
+          relativeTo: this.route
+        });
+
+      });
+    }
   }
 
 }
